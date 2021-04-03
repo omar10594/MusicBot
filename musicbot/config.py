62ed9601266daaec50352e505160821209bab5b2
Guillermo Lopez <omar10594@hotmail.com>
2021-04-02 21:03:58 -0600

    Get token from env

--- musicbot/config.py
+++ musicbot/config.py
@@ -34,7 +34,7 @@ class Config:
         self._confpreface = "An error has occured reading the config:\n"
         self._confpreface2 = "An error has occured validating the config:\n"
 
-        self._login_token = config.get("Credentials", "Token", fallback=ConfigDefaults.token)
+        self._login_token = os.environ.get('TOKEN') or config.get("Credentials", "Token", fallback=ConfigDefaults.token)
 
         self.auth = ()
 
